1077b2ff95c7073d0a65fc56d9605988b3ab6bf9
fet <kitsunefet@gmail.com>
2017-06-08 10:08:20 +0000

    beta

--- js.js
+++ js.js
@@ -1,14 +1,37 @@
-var afterLoad=require('after-load');
 const args = process.argv;
-
-var fs = require('fs');
-function readWriteSync(newValue) {
-  var data = fs.readFileSync('/var/www/html/result.txt', 'utf-8');
-  fs.writeFileSync('/var/www/html/result.txt', newValue, 'utf-8');
+var http = require('http');
+var url = require('url');
+var afterLoad=require('afterload');
+var token= args[2];
+if(!token){
+	console.log('no token found, start panic');
+	process.exit(-1);
+}
+var port = args[3];
+if(!port){
+	console.log('no port found, using default');
+	port=33333;
 }
 
-readWriteSync("");
+var server = http.createServer(function(request, response) {
+	var queryData = url.parse(request.url, true).query;
 
-var html=afterLoad(args[2]);
+//console.log('query'+queryData.url);
 
-readWriteSync(html);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	if (queryData.token===token && queryData.url) {
+		var afterLoad=require('afterload');
+		var html=afterLoad(queryData.url);
+
+		response.write(html);
+
+//		console.log(html);
+	}
+
+
+response.end();
+
+});
+
+server.listen(port);
+console.log("Server is listening at :"+port + " using token: "+token);
