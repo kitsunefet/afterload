fe47576c669e4c90853a61151ce37db7d1a74b24
kitsunefet <kitsunefet@gmail.com>
2017-06-09 19:30:03 +0500

    ffs

--- js.js
+++ js.js
@@ -17,14 +17,21 @@ var server = http.createServer(function(request, response) {
         var queryData = url.parse(request.url, true).query;
 
 //
-console.log('query: '+queryData.url);
-        
-        var check = new RegExp("^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$","i");
+//console.log('');
+//console.log('query: '+queryData.url);
+
+        var check = new RegExp('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)');
+//todo - fix regexp
+        check = RegExp('.*');
         if(!check.test(queryData.url)){
-          queryData="";
-//
-console.log("query filtered");
-        }
+         queryData="";
+
+console.log("filtered");
+response.writeHead(200, {"Content-Type": "text/html"});
+response.write("wrong query");
+response.end();
+    }
+else{
 
         response.writeHead(200, {"Content-Type": "text/html"});
         if (queryData.token===token && queryData.url) {
@@ -38,6 +45,8 @@ console.log("query filtered");
 
 
 response.end();
+}
+
 
 });
 
